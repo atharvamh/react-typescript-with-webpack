f2c3492248761532c58e5d3441078470d1875e46
Atharva Mhetar <atharva.mhetar@skypointcloud.com>
2022-11-05 15:52:49 +0530

    added webpack config for react-typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+import { upperCase } from "lodash";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p style={{color: "#000", marginLeft: "4rem", fontWeight: "bold"}}>
+          {
+            `{ ${upperCase("Edit and save to reload")} }`
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
